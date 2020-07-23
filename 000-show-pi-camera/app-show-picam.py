82c2d8bce3f6e81076f7f1c8fb599ed4f7c5b52b
imanseau <imanseau@users.noreply.github.com>
2020-07-23 17:59:58 -0500

    Rotate and Scale
    Added ability to rotate and scale image for display
    Added flags to change display options quickly at line #16

--- 000-show-pi-camera/app-show-picam.py
+++ 000-show-pi-camera/app-show-picam.py
@@ -13,12 +13,41 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
 time.sleep(0.1)
    
+# Set Flags and values for image processesing 
+scale = True
+scale_percent = 50 # Scale Percentage
+rotate = True
+rotate_degrees = 90 # Clockwise rotation (90, 180, 270)
+flip_ver = False
+flip_hor = False
+display_video = True
+
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-             
+
+    if rotate: # If True rotate by rotate_degrees
+        if rotate_degrees == 90:
+            image = cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
+        if rotate_degrees == 180:
+            image = cv.rotate(image, cv.ROTATE_180_CLOCKWISE)
+        if rotate_degrees == 270:
+            image = cv.rotate(image, cv.ROTATE_270_CLOCKWISE)
+
+    # If True flip
+    if flip_ver: image = cv.flip(image, 0)
+    if flip_hor: image = cv.flip(image, 1)
+
+    if scale: # If True scale the image
+        # calculate the new dimensions
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dsize = (width, height)
+        # resize image
+        image = cv.resize(image, dsize)
+
     # show the frame
     cv.imshow("Frame", image)
 
@@ -28,4 +57,3 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     key = cv.waitKey(1)
     if key in [27, ord('q')]:
         break
-                         
